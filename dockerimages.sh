4fc6e2047b9419c02e9f1cf6320dc0787657e0d1
Shepherd Kendall <shepherd.kendall@bethesda.net>
2023-05-03 13:55:14 -0400

    updated docker images

--- dockerimages.sh
+++ dockerimages.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
+echo "Building base image..."
 docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
+echo "Building master image..."
 docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
+echo "Building slave image..."
 docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
+echo "Building reporter image..."
 docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
